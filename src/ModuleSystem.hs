4ff975cf8dac09e699031b09311bccce642d677a
Trevor Elliott <trevor@galois.com>
2011-02-02 14:04:39 -0500

    Handle implicit module imports better
      * Translate implicit imports into actual imports, so that name clashes
        require less machinery to detect.

--- src/ModuleSystem.hs
+++ src/ModuleSystem.hs
@@ -12,7 +12,7 @@ import QualName
 import ReadWrite
 import Syntax.AST
 
-import Data.Maybe (mapMaybe,isJust,fromJust)
+import Data.Maybe (mapMaybe)
 import Data.Typeable (Typeable)
 import MonadLib
 import qualified Data.Set as Set
@@ -36,11 +36,10 @@ resolvedNames (Clash _ ns)  = ns
 -- | Merge resolved names, favoring new bound variables for shadowing.  If the
 -- boolean parameter is True, the second resolved name is assumed to be weak,
 -- and the first resolved name will be preferred.
-mergeResolved :: Bool -> Resolved -> Resolved -> Resolved
-mergeResolved isWeak a b
+mergeResolved :: Resolved -> Resolved -> Resolved
+mergeResolved a b
   | isBound a = a
   | a == b    = a
-  | isWeak    = a
   | otherwise = clash a b
 
 -- | Merge two resolved names into a name clash.
@@ -59,13 +58,10 @@ resolvedTerm (Clash n ns)  =
 type ResolvedNames = Map.Map QualName Resolved
 
 addResolvedName :: QualName -> QualName -> ResolvedNames -> ResolvedNames
-addResolvedName n rn = Map.insertWith (mergeResolved False) n (Resolved rn)
-
-addResolvedWeakName :: QualName -> QualName -> ResolvedNames -> ResolvedNames
-addResolvedWeakName n rn = Map.insertWith (mergeResolved True) n (Resolved rn)
+addResolvedName n rn = Map.insertWith mergeResolved n (Resolved rn)
 
 mergeResolvedNames :: ResolvedNames -> ResolvedNames -> ResolvedNames
-mergeResolvedNames  = Map.unionWith (mergeResolved False)
+mergeResolvedNames  = Map.unionWith mergeResolved
 
 resolve :: QualName -> Scope Term
 resolve qn = do
@@ -113,62 +109,51 @@ instance Exception NotInScope
 notFound :: QualName -> Scope a
 notFound  = raiseE . NotInScope
 
-data Use
-  = QualIdent QualName Name Bool
-  | OpenModule Open
-    deriving (Eq,Ord,Show)
+data Use = OpenModule Open Bool
+    deriving (Show,Eq,Ord)
 
 useModule :: Use -> QualName
-useModule (QualIdent qn _ _) = qn
-useModule (OpenModule o)     = openMod o
+useModule (OpenModule o _) = openMod o
 
 useRename :: Use -> Maybe QualName
-useRename (OpenModule o) = openAs o
-useRename _              = Nothing
-
--- | Given a module, find all of the modules that it uses.
-usedModules :: Module -> Set.Set Use
-usedModules m = om `Set.union` Set.map step (identModules m)
-  where
-  om                      = openModules m
-  renamed                 = Set.map fromJust
-                          $ Set.filter isJust
-                          $ Set.map useRename om
-  step (QualIdent qn n _) = QualIdent qn n (Set.member qn renamed)
-  step u                  = u
+useRename (OpenModule o _) = openAs o
 
 uniqueModules :: Set.Set Use -> Set.Set (QualName,Bool)
 uniqueModules  = Set.map step
   where
-  step (QualIdent qn _ fromRename) = (qn,fromRename)
-  step (OpenModule o)              = (openMod o, False)
+  step (OpenModule o isWeak) = (openMod o, isWeak)
 
--- | Given a module, return the modules that are referenced in its identifiers.
-identModules :: Module -> Set.Set Use
-identModules m =
-  Set.fromList (mapMaybe toModule (Set.toList (identifiers (modDecls m))))
+-- | Given a module, return the modules that are referenced in its identifiers,
+-- removing fully-qualified references to local identifiers.
+implicitlyOpenedModules :: Module -> Set.Set Use
+implicitlyOpenedModules m =
+  Set.fromList (mapMaybe toUse (Set.toList (identifiers (modDecls m))))
   where
-  toModule (QualName ps i)
+  toUse (QualName ps i)
     | ps == thisModule = Nothing
-    | len > 0          = Just (QualIdent (qualName ns n) i False)
+    | len > 0          = Just (OpenModule (Open qn (Just qn) False [i]) True)
       where
+      qn       = qualName ns n
       len      = length ps
       (ns,[n]) = splitAt (len - 1) ps
-  toModule _  = Nothing
+  toUse _  = Nothing
   thisModule  = modNamespace m
 
 -- | Given a module, return the modules that are opened by it.
-openModules :: Module -> Set.Set Use
-openModules m = Set.fromList (map OpenModule (modOpens m))
+openedModules :: Module -> Set.Set Use
+openedModules m = Set.fromList (map step (modOpens m))
+  where
+  step o = OpenModule o False
 
 -- | Run a scope checking operation with the environment created by a module.
 withEnv :: Module -> Scope a -> Scope (Interface R, a)
 withEnv m k = do
-  let uses = usedModules m
-  logDebug "Used modules"
-  logDebug (show uses)
-  iface <- loadInterfaces (Set.toList (uniqueModules uses))
-  let env0   = buildEnv iface (Set.toList uses)
+  let opened = openedModules m `Set.union` implicitlyOpenedModules m
+  logDebug "Opened modules:"
+  logDebug (show opened)
+
+  iface <- loadInterfaces (Set.toList (uniqueModules opened))
+  let env0   = buildEnv iface (Set.toList opened)
       ns     = modNamespace m
       locals = Map.fromList [ (simpleName n, Resolved (qualName ns n))
                             | d <- modDecls m, let n = declName d ]
@@ -181,22 +166,17 @@ withEnv m k = do
 loadInterfaces :: [(QualName,Bool)] -> Scope (Interface R)
 loadInterfaces  = foldM step (freezeInterface emptyInterface)
   where
-  step i (qn,fromRename) = do
+  step i (qn,isWeak) = do
     e <- try (inBase (openInterface qn))
     case e of
-      Left err | fromRename -> return i
-               | otherwise  -> raise err
-      Right i'              -> return (mergeInterfaces i i')
+      Left err | isWeak    -> return i
+               | otherwise -> raise err
+      Right i'             -> return (mergeInterfaces i i')
 
 buildEnv :: Interface R -> [Use] -> ResolvedNames
 buildEnv iface = foldr step Map.empty
   where
-  step (OpenModule o)               = mergeResolvedNames (resolveOpen iface o)
-  step (QualIdent m n fromResolved)
-    | fromResolved = addResolvedWeakName name name
-    | otherwise    = addResolvedName name name
-    where
-    name = qualName (qualPrefix m ++ [qualSymbol m]) n
+  step (OpenModule o _) = mergeResolvedNames (resolveOpen iface o)
 
 -- | Resolve an open declaration to the module names that it involves.
 resolveOpen :: Interface R -> Open -> ResolvedNames
@@ -214,7 +194,7 @@ resolveOpen iface o = rename resolved
 -- qualifications.
 resolveModule :: Interface R -> QualName -> ResolvedNames
 resolveModule iface m =
-  Map.fromListWith (mergeResolved False) (map step (modContents m iface))
+  Map.fromListWith mergeResolved (map step (modContents m iface))
   where
   step (qn,_) = (simpleName (qualSymbol qn), Resolved qn)
 
