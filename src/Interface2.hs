0591ea09682c1540c740df154a7b1bdb75d5d17d
Trevor Elliott <trevor@galois.com>
2011-07-07 12:25:20 -0700

    Flesh out the interface collection interaction a bit more

--- src/Interface2.hs
+++ src/Interface2.hs
@@ -1,20 +1,28 @@
 module Interface2 (
-    Interface()
-  , InterfaceSet()
 
-    -- * Versioning
+    -- * Interfaces
+    Interface()
+
+    -- ** Versioning
   , Version(..)
   , currentVersion
 
-    -- * Symbols
+    -- ** Symbols
   , FunSymbol(..)
 
-    -- * Creation
+    -- ** Creation
   , moduleInterface
+
+    -- * Interface Sets
+  , InterfaceSet()
+  , emptyInterfaceSet
+  , addInterface
 
     -- * Query
   , lookupFunSymbol
+  , funSymbols
   , lookupKind
+  , kinds
 
     -- * Serialization
   , interfaceFile, modInterface
@@ -36,6 +44,7 @@ import Data.Serialize
 import Data.Word (Word8)
 import System.FilePath (joinPath,(<.>),(</>))
 import qualified Data.ByteString as S
+import qualified Data.Foldable as F
 import qualified Data.Map as Map
 
 
@@ -59,6 +68,14 @@ data FunSymbol = FunSymbol
   , funType :: Forall Type
   } deriving (Show)
 
+freezeInterface :: Interface rw -> Interface R
+freezeInterface iface = Interface
+  { ifaceVersion   = ifaceVersion iface
+  , ifaceNamespace = ifaceNamespace iface
+  , ifaceTypes     = ifaceTypes iface
+  , ifaceKinds     = ifaceKinds iface
+  }
+
 
 -- Interface Collections -------------------------------------------------------
 
@@ -66,6 +83,16 @@ data FunSymbol = FunSymbol
 newtype InterfaceSet = InterfaceSet
   { interfaces :: Map.Map Namespace (Interface R)
   } deriving (Show)
+
+emptyInterfaceSet :: InterfaceSet
+emptyInterfaceSet  = InterfaceSet Map.empty
+
+-- | Insert an interface into an interface set.
+addInterface :: Interface rw -> InterfaceSet -> InterfaceSet
+addInterface iface set = set
+  { interfaces = Map.insert (ifaceNamespace iface) (freezeInterface iface)
+                     (interfaces set)
+  }
 
 
 -- Creation --------------------------------------------------------------------
@@ -120,19 +147,32 @@ class IsInterface i where
   -- | Lookup information about a symbol in an interface.
   lookupFunSymbol :: QualName -> i -> Maybe FunSymbol
 
+  -- | All of the symbols defined by an interface.
+  funSymbols :: i -> [(QualName,FunSymbol)]
+
   -- | Lookup the kind of a qualified type name.
   lookupKind :: QualName -> i -> Maybe Kind
 
+  -- | All of the kinds defined by an interface.
+  kinds :: i -> [(QualName,Kind)]
+
+
 instance IsInterface (Interface rw) where
   lookupFunSymbol qn = Map.lookup qn . ifaceTypes
-  lookupKind qn = Map.lookup qn . ifaceKinds
+  funSymbols         = Map.toList    . ifaceTypes
+  lookupKind qn      = Map.lookup qn . ifaceKinds
+  kinds              = Map.toList    . ifaceKinds
 
 instance IsInterface InterfaceSet where
   lookupFunSymbol qn rs =
     lookupFunSymbol qn =<< Map.lookup (qualPrefix qn) (interfaces rs)
 
+  funSymbols = F.foldMap funSymbols . interfaces
+
   lookupKind qn rs =
     lookupKind qn =<< Map.lookup (qualPrefix qn) (interfaces rs)
+
+  kinds = F.foldMap kinds . interfaces
 
 
 -- Serialization ---------------------------------------------------------------
