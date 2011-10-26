3ba482bc5562fffb05243963e15990b0fbd44a30
Trevor Elliott <trevor@galois.com>
2011-10-25 23:35:39 -0700

    Lambda lifting appears to be working

--- src/Compile/LambdaLift.hs
+++ src/Compile/LambdaLift.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE TupleSections #-}
 
 
 module Compile.LambdaLift (
@@ -12,12 +13,16 @@ import Dang.IO
 import Dang.Monad
 import Core.AST
 import Pretty (pretty)
-import QualName (QualName,simpleName)
+import QualName (QualName)
 import TypeChecker.Types
+import TypeChecker.Unify (Types(typeVars),quantify,Instantiate(inst))
+import Variables
+    (sccFreeQualNames,sccToList,FreeVars(),freeLocals)
 
 import Control.Applicative (Applicative,(<$>))
 import MonadLib
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 
 lambdaLift :: Module -> Dang Module
@@ -33,8 +38,11 @@ lambdaLift m = do
 -- Lambda-lifting Monad --------------------------------------------------------
 
 newtype LL a = LL
-  { unLL :: StateT Subst (ReaderT Env (WriterT [Decl] Dang)) a
+  { unLL :: StateT Rename (ReaderT Env (WriterT [Decl] Dang)) a
   } deriving (Functor,Applicative,Monad)
+
+instance ExceptionM LL SomeException where
+  raise = LL . raise
 
 instance BaseM LL Dang where
   inBase = LL . inBase
@@ -53,17 +61,17 @@ instance RunReaderM LL Env where
 
 runLL :: LL a -> Dang a
 runLL (LL m) =
-  f `fmap` runWriterT (runReaderT emptyEnv (runStateT emptySubst m))
+  f `fmap` runWriterT (runReaderT emptyEnv (runStateT emptyRename m))
   where
   f = fst . fst
 
 
--- Substitution ----------------------------------------------------------------
+-- Renaming and Lifting --------------------------------------------------------
 
-type Subst = Map.Map QualName Term
+type Rename = Map.Map QualName Term
 
-emptySubst :: Subst
-emptySubst  = Map.empty
+emptyRename :: Rename
+emptyRename  = Map.empty
 
 -- | Produce either a reference to a global variable, or the term that it is to
 -- be replaced with.
@@ -74,6 +82,23 @@ subst qn = LL $ do
     Just tm -> return tm
     Nothing -> return (Global qn)
 
+-- | Extend the closure, and return a new call-pattern for the symbol.
+addClosure :: [(Var,Type)] -> Decl -> (Decl,Term)
+addClosure clos = extend
+  where
+  tyVars    = Set.toList (typeVars (map snd clos))
+  addVars   = foldl (\f (v,ty) -> MPat (PVar v ty) . f) id clos
+  extend d  = (d',call)
+    where
+    d'   = d { declBody = quantify tyVars (addVars (forallData (declBody d))) }
+    call = app (appT (Global (declName d)) (map TVar tyVars))
+         $ map (Local . fst) clos
+
+typedClosure :: (FreeVars a) => a -> LL [(Var,Type)]
+typedClosure  = mapM step . Set.toList . freeLocals
+  where
+  step a = (a,) <$> typeOfLocal a
+
 rename :: QualName -> Term -> LL ()
 rename qn tm = LL $ do
   u <- get
@@ -82,21 +107,27 @@ rename qn tm = LL $ do
 
 -- Type Environment ------------------------------------------------------------
 
-data Name
-  = NLocal  Var
-  | NGlobal QualName
-    deriving (Eq,Show,Ord)
-
-type Env = Map.Map Name Type
+data Env = Env
+  { envParams :: Set.Set TParam
+  , envTypes  :: Map.Map Var Type
+  } deriving (Show)
 
 emptyEnv :: Env
-emptyEnv  = Map.empty
+emptyEnv  = Env
+  { envParams = Set.empty
+  , envTypes  = Map.empty
+  }
 
 addLocal :: Var -> Type -> Env -> Env
-addLocal  = Map.insert . NLocal
+addLocal n ty env = env { envTypes = Map.insert n ty (envTypes env) }
 
-addGlobal :: QualName -> Type -> Env -> Env
-addGlobal  = Map.insert . NGlobal
+addParams :: Set.Set TParam -> Env -> Env
+addParams ps env = env { envParams = ps `Set.union` envParams env }
+
+introVars :: [TParam] -> LL a -> LL a
+introVars ps k = do
+  env <- ask
+  local (addParams (Set.fromList ps) env) k
 
 introPat :: Pat -> LL a -> LL a
 introPat p k = case p of
@@ -106,6 +137,13 @@ introPat p k = case p of
     local (addLocal v ty env) k
 
   PWildcard _ -> k
+
+typeOfLocal :: Var -> LL Type
+typeOfLocal v = do
+  env <- ask
+  case Map.lookup v (envTypes env) of
+    Just ty -> return ty
+    Nothing -> fail "typeOfLocal"
 
 
 -- Core Lambda-lifting ---------------------------------------------------------
@@ -121,8 +159,10 @@ llTopDecl d = do
   b' <- llForall llMatch (declBody d)
   return d { declBody = b' }
 
-llForall :: (a -> LL a) -> Forall a -> LL (Forall a)
-llForall k (Forall ps a) = Forall ps <$> k a
+llForall :: (Show a, Types a, Instantiate a) => (a -> LL a) -> Forall a -> LL (Forall a)
+llForall k (Forall ps a) = introVars ps $ do
+  a' <- k =<< inst (map TVar ps) a
+  return (quantify ps a')
 
 llMatch :: Match -> LL Match
 llMatch m = case m of
@@ -136,7 +176,43 @@ llMatch m = case m of
 -- | Process a series of declarations from a let-expression, lifting out any
 -- that contain arguments, and generating appropriate substitutions.
 llLetDecls :: [Decl] -> LL [Decl]
-llLetDecls ds = fail "llLetDecls"
+llLetDecls  = foldM step [] . sccFreeQualNames
+  where
+  step acc ds = do
+    ds' <- llLetBlock (sccToList ds)
+    return (acc ++ ds')
+
+{-
+llLetDecl :: Decl -> LL [Decl]
+llLetDecl d = do
+  -- generate the replacement term for this declaration
+  clos <- typedClosure d
+  let (d',call) = addClosure clos d
+  rename (declName d) call
+  b' <- llForall llMatch (declBody d')
+  let decl = d' { declBody = b' }
+  if hasArgs decl
+     then put [decl] >> return []
+     else return [decl]
+     -}
+
+llLetBlock :: [Decl] -> LL [Decl]
+llLetBlock ds = do
+  clos <- typedClosure ds
+
+  let extend d = do
+        let (d',call) = addClosure clos d
+        rename (declName d') call
+        return d'
+
+      step d = do
+        b' <- llForall llMatch (declBody d)
+        let decl = d { declBody = b' }
+        if hasArgs decl
+           then put [decl] >> return []
+           else return [decl]
+
+  fmap concat . mapM step =<< mapM extend ds
 
 llTerm :: Term -> LL Term
 llTerm tm = case tm of
@@ -153,7 +229,7 @@ llTerm tm = case tm of
   Let ds e -> do
     ds' <- llLetDecls ds
     e'  <- llTerm e
-    return (Let ds' e')
+    return (letIn ds' e')
 
   Global qn -> subst qn
 
