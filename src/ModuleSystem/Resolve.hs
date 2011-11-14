884d3573060ced54bd1db1d1a8ab108514b1f8a3
Trevor Elliott <trevor@galois.com>
2011-11-14 01:22:24 -0800

    Start thinking about type and term resolution

--- src/ModuleSystem/Resolve.hs
+++ src/ModuleSystem/Resolve.hs
@@ -7,7 +7,7 @@ module ModuleSystem.Resolve (
   , mergeResolved
 
     -- * Top-level Names
-  , modDecls
+  , modResolvedNames
 
     -- * Usage Resolution
   , resolveUses
@@ -18,7 +18,8 @@ import ModuleSystem.Imports (UseSet)
 import QualName (QualName,Name,Namespace,qualName,primName,simpleName)
 import Syntax.AST
     (Module(..),modNamespace,PrimType(..),PrimTerm(..),UntypedDecl(..)
-    ,TypedDecl(..))
+    ,TypedDecl(..),DataDecl(..),Constr(..))
+import TypeChecker.Types (forallData)
 import qualified Data.ClashMap as CM
 
 
@@ -26,10 +27,15 @@ import qualified Data.ClashMap as CM
 
 -- | A clash-map from names appearing in the source to names resolved by the
 -- import environment.
-type ResolvedNames = CM.ClashMap QualName Resolved
+type ResolvedNames = CM.ClashMap UsedName Resolved
 
 -- | Lexical scope numbering.
 type Level = Int
+
+data UsedName
+  = UsedType QualName
+  | UsedTerm QualName
+    deriving (Ord,Show,Eq)
 
 -- | Names resolved by an interface, or function environment.
 data Resolved
@@ -65,38 +71,46 @@ mergeResolvedNames  = CM.unionWith mergeResolved
 -- Defined Names ---------------------------------------------------------------
 
 -- | Generate the set of resolved names that a module defines at the top-level.
-modDecls :: Module -> ResolvedNames
-modDecls m = CM.fromList
-           $ concatMap (primTypeNames ns) (modPrimTypes m)
-          ++ concatMap (primTermNames ns) (modPrimTerms m)
-          ++ concatMap (typedNames ns)    (modTyped m)
-          ++ concatMap (untypedNames ns)  (modUntyped m)
+modResolvedNames :: Module -> ResolvedNames
+modResolvedNames m = CM.fromList
+                   $ concatMap (dataDeclNames ns) (modDatas m)
+                  ++ concatMap (primTypeNames ns) (modPrimTypes m)
+                  ++ concatMap (primTermNames ns) (modPrimTerms m)
+                  ++ concatMap (typedNames ns)    (modTyped m)
+                  ++ concatMap (untypedNames ns)  (modUntyped m)
   where
   ns = modNamespace m
 
 -- | Extract the simple and qualified names that a declaration introduces.
-declResolvedNames :: (a -> Name) -> (Name -> QualName)
-                  -> (a -> [(QualName,Resolved)])
-declResolvedNames simple qualify d = [ (simpleName n, res), (qn, res) ]
+declResolvedNames :: (a -> Name) -> (QualName -> UsedName) -> (Name -> QualName)
+                  -> (a -> [(UsedName,Resolved)])
+declResolvedNames simple k qualify d = [ (k (simpleName n), res), (k qn, res) ]
   where
   n   = simple d
   qn  = qualify n
   res = Resolved 0 qn
 
--- | The resolved names from a single typed declaration.
-typedNames :: Namespace -> TypedDecl -> [(QualName,Resolved)]
-typedNames  = declResolvedNames typedName . qualName
+dataDeclNames :: Namespace -> DataDecl -> [(UsedName,Resolved)]
+dataDeclNames ns d = declResolvedNames dataName UsedType (qualName ns) d
+                  ++ concatMap (constrNames ns) (forallData (dataConstrs d))
 
-untypedNames :: Namespace -> UntypedDecl -> [(QualName,Resolved)]
-untypedNames  = declResolvedNames untypedName . qualName
+constrNames :: Namespace -> Constr -> [(UsedName,Resolved)]
+constrNames  = declResolvedNames constrName UsedTerm . qualName
+
+-- | The resolved names from a single typed declaration.
+typedNames :: Namespace -> TypedDecl -> [(UsedName,Resolved)]
+typedNames  = declResolvedNames typedName UsedTerm . qualName
+
+untypedNames :: Namespace -> UntypedDecl -> [(UsedName,Resolved)]
+untypedNames  = declResolvedNames untypedName UsedTerm . qualName
 
 -- | The resolved names form a single primitive type declaration.
-primTypeNames :: Namespace -> PrimType -> [(QualName,Resolved)]
-primTypeNames  = declResolvedNames primTypeName . primName
+primTypeNames :: Namespace -> PrimType -> [(UsedName,Resolved)]
+primTypeNames  = declResolvedNames primTypeName UsedType . primName
 
 -- | The resolved names form a single primitive term declaration.
-primTermNames :: Namespace -> PrimTerm -> [(QualName,Resolved)]
-primTermNames  = declResolvedNames primTermName . primName
+primTermNames :: Namespace -> PrimTerm -> [(UsedName,Resolved)]
+primTermNames  = declResolvedNames primTermName UsedTerm . primName
 
 
 -- UseSet Interface ------------------------------------------------------------
